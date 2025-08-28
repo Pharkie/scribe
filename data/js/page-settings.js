a7de888a9aff6648d4a1d170a475fcd00787a2e5
Adam Knowles <1413836+Pharkie@users.noreply.github.com>
2025-08-28 19:53:54 +0100

    wifi separated out

--- data/js/page-settings.js
+++ data/js/page-settings.js
@@ -330,9 +330,10 @@
         var _a, _b;
         const baseSections = [
           { id: "device", name: "Device", icon: "cpuChip", color: "blue" },
+          { id: "wifi", name: "WiFi", icon: "wifi", color: "green" },
           { id: "memos", name: "Memos", icon: "pencil", color: "pink" },
           { id: "mqtt", name: "MQTT", icon: "signal", color: "yellow" },
-          { id: "unbidden", name: "Unbidden Ink", icon: "sparkles", color: "green" },
+          { id: "unbidden", name: "Unbidden Ink", icon: "sparkles", color: "indigo" },
           { id: "buttons", name: "Buttons", icon: "arrowDownCircle", color: "teal" }
         ];
         if (((_b = (_a = this.config) == null ? void 0 : _a.leds) == null ? void 0 : _b.enabled) === true) {
